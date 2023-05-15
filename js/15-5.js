53aa4ffe25960fd4fcdf46b05875cef6c24f13ca
Tiziano26 <tizianooviedo04@gmail.com>
2023-05-15 17:38:27 -0300

    y
    clase hoy 15-5

--- js/15-5.js
+++ js/15-5.js
@@ -1,4 +1,16 @@
+let num = 545
 function ejemplo (params) {
-    let imp = document.querySelector("#")
-    console.log(imp);
+    let imp = document.querySelector("#entra");
+    let p = document.querySelector("#salida");
+    let divImg = document.querySelector("#img");
+    divImg.innerHTML = '<img src="../img/fotoperro.avif"  alt="error"/>'
+    let valor = imp.value;
+    p.innerText = valor;
+    console.log(p.value);
+    console.log(valor);
+    console.dir(imp);
+    console.log(num);
+    let r = num - "45";
+    console.log(r);
 }
+
